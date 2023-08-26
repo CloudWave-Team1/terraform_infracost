b3ca4a6e3dd8f63c921da59317b2d426af4d248c
hyeongwooo <wnguddn1234@naver.com>
2023-08-26 16:35:16 +0900

    t3.2xlarge

--- env/prod/variables.tf
+++ env/prod/variables.tf
@@ -67,7 +67,7 @@ variable "launch_template_image_id2" {
  }
 
 variable "launch_template_instance_type2" {
-  default = "c5.large"
+  default = "t3.2xlarge"
 }
 
 variable "launch_template_name_prefix2" {
