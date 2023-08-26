6317adfd47d700930345fb269647dba916770667
hyeongwooo <wnguddn1234@naver.com>
2023-08-26 16:39:11 +0900

    ccccc

--- env/prod/variables.tf
+++ env/prod/variables.tf
@@ -67,7 +67,7 @@ variable "launch_template_image_id2" {
  }
 
 variable "launch_template_instance_type2" {
-  default = "t3.2xlarge"
+  default = "c5.large"
 }
 
 variable "launch_template_name_prefix2" {
